55f003f8171cb3d5ce38d17fb18ea4daad605cc3
Jaynth746 <84017378+Jaynth746@users.noreply.github.com>
2021-06-03 16:39:33 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,7 @@
+import turtle
+
+circle = turtle.Turtle()
+for i in range(360):
+    circle.forward(1)
+    circle.left(1)
+turtle.done()
---
+++ square.py
@@ -0,0 +1,6 @@
+import turtle
+square = turtle.Turtle()
+for i in range(3):
+    square.forward(100)
+    square.right(90)
+turtle.done()
